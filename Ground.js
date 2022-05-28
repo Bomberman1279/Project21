dc826ff2d9372e722b477429bc9815f770166bcf
Bomberman1279 <92903188+Bomberman1279@users.noreply.github.com>
2022-05-27 20:04:15 -0400

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,18 @@
+class Ground{
+    constructor(x,y,w,h){
+        var groundoptions={
+            isStatic:true
+        }
+        this.body = Matter.Bodies.rectangle(x,y,w,h,groundoptions)
+        this.w= w
+        this.h= h
+        World.add(world,this.body)
+        
+    }
+
+    display(){
+        rectMode(CENTER)
+        rect(this.body.position.x,this.body.position.y,this.w,this.h)
+        
+    }
+}
